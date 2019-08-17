578668aad58e5d24e30a53d9baa941dcc664884c
Takeaki Oura <rtm6@me.com>
2019-08-18 04:54:27 +0900

    2commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+var isupdated = "false"
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/update', function (req, res) {
+  isupdated = "True"
+  res.json({
+    message: "Update!"
+  });
+});
+
+app.get('/fetch', function (req, res) {
+  res.json({
+    isupdated: isupdated
+  });
+  isupdated = "false"
+});
+
+app.listen(port, () => console.log(`Server running on ${port}`));
